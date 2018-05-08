25fe051bf07705364357b2536ff2443e4525d1a4
argiedio <argie_dio@yahoo.com>
2018-05-08 19:08:27 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
